7681b5edd6f446fee7c52806a3751a569711cdd1
simar0707 <simar0707@outlook.com>
2019-09-03 04:38:58 +0530

    add new files

--- twilio_hook.php
+++ twilio_hook.php
@@ -60,7 +60,7 @@ if(isset($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["li
 				$check_token_valid =  $handleFunctionsObject->zoho_curl($url,"GET",$data,$old_access_token);
 
 					$contactId=$check_token_valid['data'][0]['id']; 
-					$contacturl = "Contacts/".$contactId;
+					$contacturl = "Contacts";
 					 $Contactdata = '{
 								"data": [{
 								"Phone":  "'.$phone_number.'" ,
@@ -86,7 +86,7 @@ if(isset($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["li
            "Drivers1":'.$dd.'
             
 			}]}';
-			@$zohoResponse =  $handleFunctionsObject->zoho_curl($url,"PUT",$Contactdata1,$old_access_token);
+			@$Response =  $handleFunctionsObject->zoho_curl($url,"PUT",$Contactdata1,$old_access_token);
  
 					
  }
