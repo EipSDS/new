74d78f8a139bfe8a9115253617e2f5ee8433db5d
simar0707 <simar0707@outlook.com>
2019-09-03 06:58:07 +0530

    add new files

--- twilio_hook.php
+++ twilio_hook.php
@@ -82,30 +82,15 @@ if(!empty($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["l
 					"Phone":  "'.$phone_number.'" ,
 					"Last_Name":  "'.$driver_last_name.'" ,
 					"First_Name":  "'.$driver_first_name.'",
-                    "USDOT_associated_with_the_insured_s_business":  "'.$dot_number.'"	
+                    "USDOT_associated_with_the_insured_s_business":  "'.$dot_number.'",	
+                    "Name":"'.$driver_first_name.'",	
+                    "DOB_Age_MaritalStatus_Points_LicenceNo":"'.$DOB_LicenceNo.'",	
+                    "SR22":"'.$DOB_LicenceNo.'"	
 								}]}'; 
 								
 					@$contactresponse =  $handleFunctionsObject->zoho_curl($contacturl,"POST",$Contactdata,$old_access_token);
 				
- 					  if(!empty($contactresponse['data'][0]['details']['id'])){
-				    $contactId=$contactresponse['data'][0]['details']['id'];
-       
-	   $url = "Contacts/".$contactId;
- 
- 	    $DOB_LicenceNo=$DOB.','.$LicenceNo;
-		$new_array=array(
-		"DOB_Age_MaritalStatus_Points_LicenceNo"=>$DOB_LicenceNo,"SR22"=>$form_data['edit_driver_SR22'],"Name1"=>$drivername,"Back_up_Driver"=>"".$add_driver_Backup."","Owner_Driver"=>$Owner_Driver,"License_State"=>$form_data['edit_driver_license_state'],"Experience_Years"=>"".$new_driver_Exp."","Hire_Date"=>"".$Date_of_Hire.""
-		) ;
-		$driversData[0]=$new_array;
-			$dd=json_encode($driversData);
-			  $data1 = '{
-			"data": [{
-           "Drivers1":'.$dd.'
-            
-			}]}';
-			@$response =  $handleFunctionsObject->zoho_curl($url,"PUT",$data1,$old_access_token);
- 
-					  }
+
  
 				}
 
