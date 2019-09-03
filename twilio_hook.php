2c88ea6d4e9315e51aebce323948e030083ca92b
simar0707 <simar0707@outlook.com>
2019-09-03 23:49:31 +0530

    add new files

--- twilio_hook.php
+++ twilio_hook.php
@@ -40,7 +40,7 @@ $phone_number = $array["twilio"]["sms"]["From"];
  //$array["twilio"]["collected_data"]["vehicles_questions"]["answer"];
  //$status=$array["twilio"]["collected_data"]["vehicles_questions"]["status"]; 
 
-if(isset($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["license_number_of_driver"]["answer"])){
+if(ISSET($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["license_number_of_driver"]["answer"])){
  $url = "Contacts/search?phone=$phone_number";
  $data = "";
  $check_token_valid =  $handleFunctionsObject->zoho_curl($url,"GET",$data,$old_access_token);
@@ -110,7 +110,7 @@ if(isset($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["li
 				}
 
 }
-/* else{
+ else{
                     if(!empty($check_token_valid['data'][0]['id'])){
  					$contactId=$check_token_valid['data'][0]['id']; 
 					$contacturl = "Contacts/".$contactId;
@@ -160,7 +160,7 @@ if(isset($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["li
  
 				}
 				
-} */
+} 
 
 			
 }
