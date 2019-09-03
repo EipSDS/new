645e31ef7701acdf85f80951ff824c721234f4ab
simar0707 <simar0707@outlook.com>
2019-09-03 07:00:27 +0530

    add new files

--- twilio_hook.php
+++ twilio_hook.php
@@ -82,10 +82,7 @@ if(!empty($array["twilio"]["collected_data"]["vehicles_questions"]["answers"]["l
 					"Phone":  "'.$phone_number.'" ,
 					"Last_Name":  "'.$driver_last_name.'" ,
 					"First_Name":  "'.$driver_first_name.'",
-                    "USDOT_associated_with_the_insured_s_business":  "'.$dot_number.'",	
-                    "Name":"'.$driver_first_name.'",	
-                    "DOB_Age_MaritalStatus_Points_LicenceNo":"'.$DOB_LicenceNo.'",	
-                    "SR22":"'.$DOB_LicenceNo.'"	
+                    "USDOT_associated_with_the_insured_s_business":  "'.$dot_number.'"
 								}]}'; 
 								
 					@$contactresponse =  $handleFunctionsObject->zoho_curl($contacturl,"POST",$Contactdata,$old_access_token);
