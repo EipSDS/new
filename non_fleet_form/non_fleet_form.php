60f251d6de68c81b210f00343e7f940c3cef3578
simar0707 <simar0707@outlook.com>
2019-09-19 06:42:18 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -20,7 +20,7 @@ if($contact_id!== ''){
 				$url = "Contacts/search?$phone_number";
 			$data = "";
 			echo $check_token_valid =  $handleFunctionsObject->zoho_curl($url,"GET",$data,$old_access_token);
-			if(ISSET($check_token_valid['code']) && $check_token_valid['code'] !== "INVALID_TOKEN"){
+			if(ISSET($check_token_valid['code']) && $check_token_valid['code'] == "INVALID_TOKEN"){
 				$url = "token";
 				$data = array("refresh_token"=>$refresh_token,"client_id"=>"".$zoho_client_id."","client_secret"=>"".$zoho_client_secret."","grant_type"=>"refresh_token");
 				$get_new_token = $handleFunctionsObject-> zoho_auth($url,"POST",$data);
