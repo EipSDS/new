442c1f98db132927239760068a830d33fa823797
simar0707 <simar0707@outlook.com>
2019-09-20 07:51:47 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -58,11 +58,13 @@ echo "<pre>";
 print_r($check_token_valid);
 echo "</pre>";	
 
-$query = "SELECT * FROM public.contact_commodities where contact_id=".$contact_id."";
-$rs = pg_query($query);
+$query = "SELECT * FROM public.contact_commodities where contact_id='4098623000001221042'";
+$result = pg_query($query);
 if($result){
+	while($row = $result->fetch_assoc()) {
 	echo "connected susccessfully";
-	echo $result['value'];
+	echo $row['value'];
+}
 }
 else{
 	echo "not connected successfully";
