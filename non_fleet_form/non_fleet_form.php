305896a3ffd9e7309b48aea45238bcb34a86b753
simar0707 <simar0707@outlook.com>
2019-09-21 04:20:28 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -1309,16 +1309,16 @@ else{
 	</tr>
 	</table>
 <?php
-$query2 = "SELECT * FROM public.contact_vehicles where contact_id='$contact_id' AND vehicle_type='1981 or newer vehicle'";
-$result2 = pg_query($query2);
-$rows2 = pg_num_rows($result2);
-if($rows2>=1){
-while ($row2 = pg_fetch_assoc($result2)) { 
+$query21 = "SELECT * FROM public.contact_vehicles where contact_id='$contact_id' AND vehicle_type='1981 or newer vehicle'";
+$result21 = pg_query($query21);
+$rows21 = pg_num_rows($result21);
+if($rows21>=1){
+while ($row21 = pg_fetch_assoc($result21)) { 
 ?>
 	<table align="left" width="100%"  cellpadding="6" cellspacing="0" bgcolor="#ffffff" style="font-size:12px;border: 1px solid #000000;">
 	<tr>
 	<td>
-	<input type="text" name="Tractors1" class='tractors' id="tractors1" value="<?php echo $row2['make']; ?>" width="100%" style="width:100%;border: 0;font-size:14px;"/>
+	<input type="text" name="Tractors1" class='tractors' id="tractors1" value="<?php echo $row21['make']; ?>" width="100%" style="width:100%;border: 0;font-size:14px;"/>
 	</td>
 	</tr>
 	</table>
