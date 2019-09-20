6fb98620e0397f347866097de485e4b5508c0efe
simar0707 <simar0707@outlook.com>
2019-09-20 08:23:59 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -60,16 +60,15 @@ echo "</pre>";
 
 $query = "SELECT * FROM public.contact_commodities where contact_id='".$contact_id."'";
 $result = pg_query($query);
-	if($result){
-	while($row = mysqli_fetch_assoc($result)) {
-	echo "Record Created Sucessfully";
-	echo $row['value'];
-	}
-	}
-	else
-	{
-	echo "failed to create";
-	} 	
+	$rows = pg_num_rows($result);
+		if($rows>=1){
+			while ($row = pg_fetch_assoc($result)) {
+			 echo $row['value'];
+			}
+		}else{
+		echo "not connected";
+			
+		}
 	
 }
 else{
