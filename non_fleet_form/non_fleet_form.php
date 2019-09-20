be39570a25d2db02d462bed45b35e32da8561eb2
simar0707 <simar0707@outlook.com>
2019-09-21 03:42:18 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -1346,18 +1346,13 @@ $result3 = pg_query($query3);
 $rows3 = pg_num_rows($result3);
 if($rows3>=1){
 	while ($row3 = pg_fetch_assoc($result3)) {
-	 echo $row3['year'];
-	 echo '<br>';	
-	 echo $row3['make'];
-	 echo '<br>';	
-	 echo $row3['vin'];
-	 echo '<br>';	 
+ 
 
 ?>
 	<table align="left" width="100%"  cellpadding="6" cellspacing="0" bgcolor="#ffffff" style="font-size:12px;border: 1px solid #000000;">
 	<tr>
 	<td>
-	<input type="text" name="VIN1" class='vin' id="vin1" width="100%"  value="test" style="width:100%;border: 0;font-size:14px;"/>
+	<input type="text" name="VIN1" class='vin' id="vin1" width="100%"  value="<?php echo $row3['year']; ?>" style="width:100%;border: 0;font-size:14px;"/>
 	</td>
 	</tr>
 	</table>
