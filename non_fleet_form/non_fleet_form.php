d1f1ab222e73c2ba8f486297d5d2e5abc60345be
simar0707 <simar0707@outlook.com>
2019-09-24 07:04:00 +0530

    add new page

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -135,7 +135,8 @@ echo $Radious_600_miles=$check_token_valid['data'][0]['Radious_600_miles'];
 // echo $driver_Name1=$check_token_valid['data'][0]['Drivers1'][0]['Name1'];
 // echo $DOB_Age_MaritalStatus_Points_LicenceNo=$check_token_valid['data'][0]['Drivers1'][0]['DOB_Age_MaritalStatus_Points_LicenceNo'];
 
-$count=count($check_token_valid['data'][0]['Drivers1']);
+echo $count=count($check_token_valid['data'][0]['Drivers1']);
+echo "exit;"
 for($i = 0; $i<$count; $i++){
 echo $drivername=$check_token_valid['data'][0]['Drivers1'][$i]['Name1'];
  $DOB_Age_MaritalStatus_Points_LicenceNo=$check_token_valid['data'][0]['Drivers1'][$i]['DOB_Age_MaritalStatus_Points_LicenceNo'];
