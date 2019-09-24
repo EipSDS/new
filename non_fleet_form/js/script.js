fea402046ac391f8c410770088d4edbd8ecdcfba
simar0707 <simar0707@outlook.com>
2019-09-25 02:08:07 +0530

    add new files

--- non_fleet_form/js/script.js
+++ non_fleet_form/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $(".save").click(function(){
     alert("The paragraph was clicked.");
   });
+  
+  var dataform=	$('.dataform').find('select, textarea, input').serialize();
+   console.log(dataform);
+   alert(dataform);
+  
 });
