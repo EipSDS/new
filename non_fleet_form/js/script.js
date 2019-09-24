927cb06ff1aa5825dfbce3f580dbc5ea3122c660
simar0707 <simar0707@outlook.com>
2019-09-25 04:26:44 +0530

    add new files

--- non_fleet_form/js/script.js
+++ non_fleet_form/js/script.js
@@ -2,28 +2,16 @@
  $(document).ready(function(){
   $('.save').click(function(){
   var contact_id =$(".ContactId").val();
-//console.log(contact_id);
   var dataform=	$('.dataform').find('select, textarea, input').serialize();
   var url = "http://givesurance.herokuapp.com/non_fleet_form/ajaxdata.php";
-  alert(url);
-//console.log(dataform);
-   $.ajax({
-   url:url,
-   type: "POST",   
-   dataType: 'json',
-   data: {savedata: "success",contact_id:contact_id, dataform:dataform},
-   success:function(result){		
-    //  content.html(result);
-	  console.log(result);
-   }	   
-   });
    $.ajax({
       type: "POST",
       url: url,
       data: {hi:'hi',contact_id:contact_id, dataform:dataform},
       dataType: "text",
       success: function(resultData){
-          alert(resultData);
+         alert(resultData);
+		 console.log(resultData);
       }
 });
     });
