9dbe9852a4e4fd5fa79821121c84af7395534a8b
simar0707 <simar0707@outlook.com>
2019-09-25 04:06:23 +0530

    add new files

--- non_fleet_form/js/script.js
+++ non_fleet_form/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $(".save").click(function(event){
   var contact_id =$(".ContactId").val();
-console.log(contact_id);
+//console.log(contact_id);
   var dataform=	$('.dataform').find('select, textarea, input').serialize();
-console.log(dataform);
+//console.log(dataform);
    $.ajax({
    url:"ajaxdata.php",
    type: "POST",   
-   dataType: 'text',
+   dataType: 'json',
    data: ({savedata: "success",contact_id:contact_id, dataform:dataform}),
    success:function(result){		
     //  content.html(result);
