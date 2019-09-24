9954e3892b3cff25468d26117fe0386258e50f80
simar0707 <simar0707@outlook.com>
2019-09-25 03:05:52 +0530

    add new files

--- non_fleet_form/js/script.js
+++ non_fleet_form/js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
   $(".save").click(function(){
-var thanks= "Data saved";
   var contact_id =$(".ContactId").val();
   
   var dataform=	$('.dataform').find('select, textarea, input').serialize();
    console.log(contact_id);
-   alert(contact_id);
+
    $.ajax({
    url:"ajaxdata.php",
    type: "POST",   
@@ -13,6 +12,7 @@ var thanks= "Data saved";
    data : ({savedata:"success",contact_id:contact_id, dataform:dataform}),
    success:function(result){
    $(".save_success").show();
+      alert(contact_id);
    }	   
    })
     });
