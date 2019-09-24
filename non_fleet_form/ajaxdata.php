0629ba6b0967d4751d0834e23f23621b1e0bbe39
simar0707 <simar0707@outlook.com>
2019-09-25 04:53:13 +0530

    add new files

--- non_fleet_form/ajaxdata.php
+++ non_fleet_form/ajaxdata.php
@@ -1,5 +1,7 @@
 <?php
 print($_POST['dataform']);
+echo $_POST['contactId'];
+echo $_POST['Tractors2'];
 /* exit();
 	if(ISSET($_POST['savedata']) && $_POST['savedata']=='success'){
 		echo $data = json_encode($_POST);
