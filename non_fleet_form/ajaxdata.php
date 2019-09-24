926b0fe7fa6771f10f6977d21475cc2266381344
simar0707 <simar0707@outlook.com>
2019-09-25 04:46:47 +0530

    add new files

--- non_fleet_form/ajaxdata.php
+++ non_fleet_form/ajaxdata.php
@@ -1,7 +1,5 @@
 <?php
-echo $contact=$_POST['savedata'];
-echo $contact=$_POST['hi'];
-print_r($_POST);
+$_POST;
 /* exit();
 	if(ISSET($_POST['savedata']) && $_POST['savedata']=='success'){
 		echo $data = json_encode($_POST);
