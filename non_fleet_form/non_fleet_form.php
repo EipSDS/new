04aa94c7126f6adc7ae5ca383a0758f4fb2c3405
simar0707 <simar0707@outlook.com>
2019-09-25 03:03:31 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -3073,9 +3073,9 @@ if($rows>=1){
 		</td>
 	</tr>
 	</table>
-		<div role="alert" class='save_success' style='display:none;color:green';>
+		<span  class='save_success' style='display:none;color:green';>
 		Please Fill all fields.
-	</div>
+	</span>
 	<input type="hidden" name="contactId" value="<?php echo $_GET['contact_id']; ?>" placeholder="Phone Number" required class='contactId' />
 	</td>
 	</tr>
