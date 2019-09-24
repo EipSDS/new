e56b835200bbbc7c64d4d17e3a37878fdab37740
simar0707 <simar0707@outlook.com>
2019-09-25 02:42:08 +0530

    add new files

--- non_fleet_form/ajaxdata.php
+++ non_fleet_form/ajaxdata.php
@@ -1,11 +1,11 @@
 <?php
 
 	if(ISSET($_POST['CargoRelated_next']) && $_POST['CargoRelated_next']=='success'){
-		echo json_encode($_POST);
-	echo '<script type="text/javascript">alert("' . $_POST . '")</script>';
+		echo $data = json_encode($_POST);
+	echo '<script type="text/javascript">alert("' . $data . '")</script>';
 	
 echo '<script language="javascript">';
-echo 'alert("'.$_POST.'")';
+echo 'alert("'.$data.'")';
 echo '</script>';
 	}
 	
