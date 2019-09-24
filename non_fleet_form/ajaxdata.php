ec216ce6d081a6b405e63eaeaa179ce179213554
simar0707 <simar0707@outlook.com>
2019-09-25 03:19:29 +0530

    add new files

--- non_fleet_form/ajaxdata.php
+++ non_fleet_form/ajaxdata.php
@@ -1,12 +1,7 @@
 <?php
-
+echo $_POST;
 	if(ISSET($_POST['savedata']) && $_POST['savedata']=='success'){
 		echo $data = json_encode($_POST);
-	echo '<script type="text/javascript">alert("' . $data . '")</script>';
-	
-echo '<script language="javascript">';
-echo 'alert("'.$data.'")';
-echo '</script>';
 	}
 	
 ?>
