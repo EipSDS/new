f8a811689d2ec7b5f3f0d5ae0ad907e9d43898a7
simar0707 <simar0707@outlook.com>
2019-09-26 03:00:32 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -557,7 +557,7 @@ if($rows>=1){
 			<table align="left" cellpadding="6" cellspacing="0">
 				<tr>
 					<td align="center">
-						<input type="text" name="Effective Date" class=" datepicker current_policy_Effective_date" id="effective_date" value="<?php echo $effective_date; ?>" width="100%" style="width:125px;border: 0;font-size:14px;"/>
+						<input type="text" name="Effective Date" class=" datepicker" id="effective_date" value="<?php echo $effective_date; ?>" width="100%" placeholder="<?php echo date('m/d/Y');?>" style="width:125px;border: 0;font-size:14px;"/>
 					</td>
 				</tr>
 			</table>
