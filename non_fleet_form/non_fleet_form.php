cf922cfd3a4cb85906ea5a497aa4483e57306d49
simar0707 <simar0707@outlook.com>
2019-09-26 22:58:27 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -2627,7 +2627,7 @@ if($rows22>=1){
 	<tr>
 	<td>
 	<input type="text" name="Units_box2" class="Units" id="Units_box2" width="100%" value="<?php echo $array3_units[1]; ?>" style="width:100%;border: 0;font-size:14px;"/>
-	<input type="hidden" name="Units_box2id" class="Units_box2id" id="Units_box2id" value="<?php echo $array3_id[1]; ?>" />
+	<input type="hidden" name="Units_box2id" class="Units_box2id" id="Units_box2id" value="<?php echo $array3_id[1]; ?>"/>
 	</td>
 	</tr>
 	</table>
@@ -2635,7 +2635,7 @@ if($rows22>=1){
 	<tr>
 	<td>
 	<input type="text" name="Units_box3" class='Units' id="Units_box3" value="<?php echo $array3_units[2]; ?>" width="100%" style="width:100%;border: 0;font-size:14px;"/>
-	<input type="hidden" name="Units_box3id" class="Units_box3id" id="Units_box3id" value="<?php echo $array3_id[2]; ?>" />
+	<input type="hidden" name="Units_box3id" class="Units_box3id" id="Units_box3id" value="<?php echo $array3_id[2]; ?>"/>
 	</td>
 	</tr>
 	</table>
@@ -2643,7 +2643,7 @@ if($rows22>=1){
 	<tr>
 	<td>
 	<input type="text" name="Units_box4" class='Units' id="Units_box4" value="<?php echo $array3_units[3]; ?>" width="100%" style="width:100%;border: 0;font-size:14px;"/>
-	<input type="hidden" name="Units_box4id" class="Units_box4id" id="Units_box4id" value="<?php echo $array3_id[3]; ?>" />
+	<input type="hidden" name="Units_box4id" class="Units_box4id" id="Units_box4id" value="<?php echo $array3_id[3]; ?>"/>
 	</td>
 	</tr>
 	</table>
