2a8d887fa9359d5c04ab11200b6575192ca89cc7
simar0707 <simar0707@outlook.com>
2019-09-26 07:41:13 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -2183,7 +2183,7 @@ if($rows>=1){
 	<table align="left" width="100%"  cellpadding="6" cellspacing="0" bgcolor="#ffffff" style="font-size:12px;border: 1px solid #000000;">
 	<tr>
 	<td>
-	<input type="text" name="birth2" value="<?php echo $DOB1; ?>" class='birth' id="birth1" width="100%" style="width:100%;border: 0;font-size:14px;"/>
+	<input type="text" name="birth2" value="<?php echo $DOB1; ?>" class='birth' id="birth2" width="100%" style="width:100%;border: 0;font-size:14px;"/>
 	</td>
 	</tr>
 	</table>
