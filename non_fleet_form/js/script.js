e5eac429924570eac2aa1a2db55bcecdd192b48e
simar0707 <simar0707@outlook.com>
2019-09-27 04:24:31 +0530

    add new files

--- non_fleet_form/js/script.js
+++ non_fleet_form/js/script.js
@@ -52,6 +52,16 @@
  var make4=$("#make4").val(); 
  var Tractors5id=$("#Tractors5id").val(); 
  var Tractors5=$("#Tractors5").val(); 
+ console.log(Tractors2id);
+ console.log(Tractors3id);
+ console.log(Tractors4id);
+ console.log(Tractors5id);
+  console.log(Tractors1);
+ console.log(Tractors2);
+ console.log(Tractors3);
+ console.log(Tractors4);
+ console.log(Tractors5);
+ 
  var make5=$("#make5").val(); 
  var VIN1=$("#VIN1").val(); 
  var vin2=$("#vin2").val(); 
@@ -176,11 +186,7 @@
  var loss1id=$("#loss1id").val(); 
  var loss2id=$("#loss2id").val(); 
  var loss3id=$("#loss3id").val(); 
- console.log(Commodity1);
- console.log(hauled1);
- console.log(hauled2);
- console.log(Commodity2);
- console.log(Commodity1id);
+
  var Power_new2=$("#Power_new2").val(); 
  var Power_new3=$("#Power_new3").val(); 
  var Power_new4=$("#Power_new4").val(); 
@@ -200,16 +206,16 @@
  var Motor_Truck5=$("#Motor_Truck5").val(); 
  var Motor_Truck6=$("#Motor_Truck6").val(); 
 
-  var url = "http://givesurance.herokuapp.com/non_fleet_form/ajaxdata.php";
-   $.ajax({
-      type: "POST",
-      url: url,
-      data: {savedata:'success',contact_id:contact_id, submitting_agency:submitting_agency,Contact_Person:Contact_Person, Applicant_Name:Applicant_Name,Applicant_lastName:Applicant_lastName, garaging_address:garaging_address, dot:dot, City:City, State:State, Zip:Zip, years_in_bus:years_in_bus, mailing_address:mailing_address, City2:City2, State2:State2, Zip2:Zip2, contact_name:contact_name, Phone:Phone, e_mail:e_mail, miles:miles, miles_2:miles_2, miles_3:miles_3, miles_4:miles_4, major:major, csl:csl, um_uim:um_uim, pip:pip, limit:limit, motor_deductible:motor_deductible, cost:cost, effective_date:effective_date, payroll:payroll, employees:employees, owners:owners, limit:limit, trailers:trailers,  active:active, Tractors1id:Tractors1id, Tractors1:Tractors1, make1:make1, Tractors2id:Tractors2id, Tractors2:Tractors2, make2:make2, Tractors3id:Tractors3id, Tractors3:Tractors3, make3:make3, Tractors4id:Tractors4id, Tractors4:Tractors4, make4:make4, Tractors5id:Tractors5id, Tractors5:Tractors5, make5:make5, VIN1:VIN1, vin2:vin2, vin3:vin3, vin4:vin4, vin5:vin5, Stated1:Stated1, Stated2:Stated2,  Stated3:Stated3, Stated4:Stated4, Stated5:Stated5, trailer1id:trailer1id,   trailer1:trailer1, trailermake1:trailermake1, trailer2id:trailer2id, trailermake2:trailermake2, trailer3id:trailer3id, trailer3:trailer3, trailermake3:trailermake3, trailer4id:trailer4id, trailer4:trailer4, trailermake4:trailermake4, trailer5id:trailer5id, trailer5:trailer5, ntrailermake5:trailermake5, VIN_new:VIN_new, VIN_new2:VIN_new2, VIN_new3:VIN_new3, VIN_new4:VIN_new4, VIN_new5:VIN_new5, Amount1:Amount1, Amount2:Amount2, Amount3:Amount3, Amount4:Amount4, Amount5:Amount5, name1id:name1id, name2id:name2id, name3id:name3id, name4id:name4id, name5id:name5id, Name1:Name1, name2:name2, name3:name3, name4:name4, name5:name5, eo1:eo1, eo2:eo2, eo3:eo3, eo4:eo4, eo5:eo5, License_State:License_State, License_State1:License_State1, License_State2:License_State2, License_State3:License_State3, License_State4:License_State4, state_license1:state_license1,  state_license2:state_license2, state_license3:state_license3, state_license4:state_license4, state_license5:state_license5, Years_of_Experience1:Years_of_Experience1, experience2:experience2, experience3:experience3, experience4:experience4, experience5:experience5, birth1:birth1, birth2:birth2, birth3:birth3, birth4:birth4, birth5:birth5, date1:date1, date2:date2, date3:date3, date4:date4, date5:date5, Violations1:Violations1, Violations2:Violations2, Violations3:Violations3, Violations4:Violations4, Violations5:Violations5, Commodity1id:Commodity1id, Commodity1:Commodity1, Commodity2id:Commodity2id, Commodity2:Commodity2, Commodity3id:Commodity3id, Commodity3:Commodity3, Commodity4id:Commodity4id, Commodity4:Commodity4, Commodity5id:Commodity5id, Commodity5:Commodity5, hauled1:hauled1, hauled2:hauled2, hauled3:hauled3,	hauled4:hauled4, hauled5:hauled5, Value1:Value1, Value2:Value2, Value3:Value3, Value4:Value4, Value5:Value5, Stated_Amount1:Stated_Amount1, Stated_Amount2:Stated_Amount2, Stated_Amount3:Stated_Amount3, Stated_Amount4:Stated_Amount4, Stated_Amount5, Units_box1id:Units_box1id, Units_box2id:Units_box2id, Units_box3id:Units_box3id, Units_box4id:Units_box4id,  Units_box1:Units_box1, Units_box2:Units_box2, Units_box3:Units_box3, Units_box4:Units_box4, Total_Miles1:Total_Miles1, Total_Miles2:Total_Miles2, Total_Miles3:Total_Miles3, Total_Miles4:Total_Miles4, Receipts1:Receipts1, Receipts2:Receipts2, Receipts3:Receipts3, Receipts4:Receipts4, loss1id:loss1id, loss2id:loss2id, loss2id:loss2id,loss3id:loss3id, Power_new2:Power_new2, Power_new3:Power_new3, Power_new4:Power_new4, Total_Incurred1:Total_Incurred1, Total_Incurred2:Total_Incurred2, Total_Incurred3:Total_Incurred3, Losses_Damage1:Losses_Damage1, Losses_Damage2:Losses_Damage2, Losses_Damage3:Losses_Damage3, Total_Incurred_1:Total_Incurred_1, Total_Incurred_2:Total_Incurred_2, Total_Incurred_3:Total_Incurred_3, Motor_Truck1:Motor_Truck1, Motor_Truck2:Motor_Truck2, Motor_Truck3:Motor_Truck3, Motor_Truck4:Motor_Truck4, Motor_Truck5:Motor_Truck5, Motor_Truck6:Motor_Truck6},
-      dataType: "text",
-      success: function(resultData){
-		 console.log(resultData);
-		alert(resultData);
-      }
-});
+  // var url = "http://givesurance.herokuapp.com/non_fleet_form/ajaxdata.php";
+   // $.ajax({
+      // type: "POST",
+      // url: url,
+      // data: {savedata:'success',contact_id:contact_id, submitting_agency:submitting_agency,Contact_Person:Contact_Person, Applicant_Name:Applicant_Name,Applicant_lastName:Applicant_lastName, garaging_address:garaging_address, dot:dot, City:City, State:State, Zip:Zip, years_in_bus:years_in_bus, mailing_address:mailing_address, City2:City2, State2:State2, Zip2:Zip2, contact_name:contact_name, Phone:Phone, e_mail:e_mail, miles:miles, miles_2:miles_2, miles_3:miles_3, miles_4:miles_4, major:major, csl:csl, um_uim:um_uim, pip:pip, limit:limit, motor_deductible:motor_deductible, cost:cost, effective_date:effective_date, payroll:payroll, employees:employees, owners:owners, limit:limit, trailers:trailers,  active:active, Tractors1id:Tractors1id, Tractors1:Tractors1, make1:make1, Tractors2id:Tractors2id, Tractors2:Tractors2, make2:make2, Tractors3id:Tractors3id, Tractors3:Tractors3, make3:make3, Tractors4id:Tractors4id, Tractors4:Tractors4, make4:make4, Tractors5id:Tractors5id, Tractors5:Tractors5, make5:make5, VIN1:VIN1, vin2:vin2, vin3:vin3, vin4:vin4, vin5:vin5, Stated1:Stated1, Stated2:Stated2,  Stated3:Stated3, Stated4:Stated4, Stated5:Stated5, trailer1id:trailer1id,   trailer1:trailer1, trailermake1:trailermake1, trailer2id:trailer2id, trailermake2:trailermake2, trailer3id:trailer3id, trailer3:trailer3, trailermake3:trailermake3, trailer4id:trailer4id, trailer4:trailer4, trailermake4:trailermake4, trailer5id:trailer5id, trailer5:trailer5, ntrailermake5:trailermake5, VIN_new:VIN_new, VIN_new2:VIN_new2, VIN_new3:VIN_new3, VIN_new4:VIN_new4, VIN_new5:VIN_new5, Amount1:Amount1, Amount2:Amount2, Amount3:Amount3, Amount4:Amount4, Amount5:Amount5, name1id:name1id, name2id:name2id, name3id:name3id, name4id:name4id, name5id:name5id, Name1:Name1, name2:name2, name3:name3, name4:name4, name5:name5, eo1:eo1, eo2:eo2, eo3:eo3, eo4:eo4, eo5:eo5, License_State:License_State, License_State1:License_State1, License_State2:License_State2, License_State3:License_State3, License_State4:License_State4, state_license1:state_license1,  state_license2:state_license2, state_license3:state_license3, state_license4:state_license4, state_license5:state_license5, Years_of_Experience1:Years_of_Experience1, experience2:experience2, experience3:experience3, experience4:experience4, experience5:experience5, birth1:birth1, birth2:birth2, birth3:birth3, birth4:birth4, birth5:birth5, date1:date1, date2:date2, date3:date3, date4:date4, date5:date5, Violations1:Violations1, Violations2:Violations2, Violations3:Violations3, Violations4:Violations4, Violations5:Violations5, Commodity1id:Commodity1id, Commodity1:Commodity1, Commodity2id:Commodity2id, Commodity2:Commodity2, Commodity3id:Commodity3id, Commodity3:Commodity3, Commodity4id:Commodity4id, Commodity4:Commodity4, Commodity5id:Commodity5id, Commodity5:Commodity5, hauled1:hauled1, hauled2:hauled2, hauled3:hauled3,	hauled4:hauled4, hauled5:hauled5, Value1:Value1, Value2:Value2, Value3:Value3, Value4:Value4, Value5:Value5, Stated_Amount1:Stated_Amount1, Stated_Amount2:Stated_Amount2, Stated_Amount3:Stated_Amount3, Stated_Amount4:Stated_Amount4, Stated_Amount5, Units_box1id:Units_box1id, Units_box2id:Units_box2id, Units_box3id:Units_box3id, Units_box4id:Units_box4id,  Units_box1:Units_box1, Units_box2:Units_box2, Units_box3:Units_box3, Units_box4:Units_box4, Total_Miles1:Total_Miles1, Total_Miles2:Total_Miles2, Total_Miles3:Total_Miles3, Total_Miles4:Total_Miles4, Receipts1:Receipts1, Receipts2:Receipts2, Receipts3:Receipts3, Receipts4:Receipts4, loss1id:loss1id, loss2id:loss2id, loss2id:loss2id,loss3id:loss3id, Power_new2:Power_new2, Power_new3:Power_new3, Power_new4:Power_new4, Total_Incurred1:Total_Incurred1, Total_Incurred2:Total_Incurred2, Total_Incurred3:Total_Incurred3, Losses_Damage1:Losses_Damage1, Losses_Damage2:Losses_Damage2, Losses_Damage3:Losses_Damage3, Total_Incurred_1:Total_Incurred_1, Total_Incurred_2:Total_Incurred_2, Total_Incurred_3:Total_Incurred_3, Motor_Truck1:Motor_Truck1, Motor_Truck2:Motor_Truck2, Motor_Truck3:Motor_Truck3, Motor_Truck4:Motor_Truck4, Motor_Truck5:Motor_Truck5, Motor_Truck6:Motor_Truck6},
+      // dataType: "text",
+      // success: function(resultData){
+		 // console.log(resultData);
+		// alert(resultData);
+      // }
+// });
     });
 });
