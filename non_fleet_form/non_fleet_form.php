dae26cb9d230f279eb2736d13d4e4ac09197ff64
simar0707 <simar0707@outlook.com>
2019-09-28 06:31:26 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -3470,11 +3470,10 @@ $(document).ready(function(){
 		</td>
 	</tr>
 	</table>
-	<table width="100%" align="center" class="save_success" style='display:none;color:green align:center'; height="10" cellpadding="0" cellspacing="0">
-		<span  style='color:green align:center';>
+		<span width="100%" class='save_success' style='display:none;color:green align:center';>
 		Data saved
 	</span>
-</table>
+
 	<input type="hidden" name="contactId" value="<?php echo $_GET['contact_id']; ?>" placeholder="Phone Number" required class='contactId' />
 	</td>
 	</tr>
