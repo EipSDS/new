fc91d13ffe42e409d872d1481d212e690d4d3cf3
simar0707 <simar0707@outlook.com>
2019-09-30 23:44:18 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -14,11 +14,15 @@ $phone_number=$_GET['phone'];
 if(isset($_POST['submit'])){
 
 require("fpdf.php");
-$pdf=new FPDF();
+$pdf = new FPDF(); 
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,http://www.fpdf.org);
-$pdf->Output();
+$pdf->SetFillColor(1,99,255); // input R ,G , B 
+$pdf->SetTextColor(255,254,254);// input R , G , B 
+$pdf->SetDrawColor(255,1,1);// input R , G , B 
+$pdf->SetLineWidth(1);
+$pdf->Cell(80,10,'Hello World!',1,0,C,true,'https://www.plus2net.com');
+$pdf->Output('my_file.pdf','I'); // Send to browser and display
 	
 }
 
