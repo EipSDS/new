36ac11b17e4acbed361da70a85f0220368bc506a
simar0707 <simar0707@outlook.com>
2019-09-30 22:18:32 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -18,7 +18,7 @@ require("fpdf.php");
 $pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(80,10,'Hello World!',1,0,C,true,'https://www.plus2net.com');
 $pdf->Output();
 
 }
