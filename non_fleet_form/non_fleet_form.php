b76a5159845b21be6c5f88312488d8c61e15f816
simar0707 <simar0707@outlook.com>
2019-09-30 22:54:26 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -12,24 +12,6 @@ $phone_number;
 $phone_number=$_GET['phone'];
 
 if(isset($_POST['submit']){
-    require('phpToPDF.php');
-
-    //It is possible to include a file that outputs html and store it in a variable 
-    //using output buffering.
-    ob_start();
-    include('/examples/file.php');
-    $my_html = ob_get_clean();
-
-    //Set Your Options -- we are saving the PDF as 'my_filename.pdf' to a 'my_pdfs' folder
-    $pdf_options = array(
-      "source_type" => 'html',
-      "source" => $my_html,
-      "action" => 'save',
-      "save_directory" => 'my_pdfs',
-      "file_name" => 'my_filename.pdf');
-
-    //Code to generate PDF file from options above
-    phptopdf($pdf_options);
 	
 	
 }
