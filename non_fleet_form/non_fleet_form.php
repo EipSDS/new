21349193bf8963c229bf8ade38d70ae3d492dadb
simar0707 <simar0707@outlook.com>
2019-09-30 23:40:25 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -11,13 +11,13 @@ $contact_id;
 $phone_number;
 $phone_number=$_GET['phone'];
 
-if(isset($_POST['submit']){
+if(isset($_POST['submit'])){
 
 require("fpdf.php");
 $pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,'http://www.fpdf.org');
 $pdf->Output();
 	
 }
