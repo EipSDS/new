7e33cee1719d9424aeba171bafa1f5754ca0354f
simar0707 <simar0707@outlook.com>
2019-10-01 21:52:51 +0530

    add new file

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -13,17 +13,44 @@ $phone_number=$_GET['phone'];
 
 if(isset($_POST['submit'])){
 
- require("fpdf.php");
-    $the_file       = "http://givesurance.herokuapp.com/non_fleet_form/non_fleet_form.php/?phone=4098623000001221042&contact_id=4098623000001368004/";
-    $myfile         = fopen($the_file, "r") or die("Unable to open file!!!!<br><br><br>");
-    $homepage     = file_get_contents($the_file);
-    fclose($myfile);
-    $pdf = new FPDF();
-    $pdf->AddPage();
-    $pdf->SetFont('Arial','B',9);
-    $pdf->Cell(40,10, $homepage);
+require('fpdf.php');
 
-    $pdf->Output();
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->Cell(30,10,'Title',1,0,'C');
+    // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Page number
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+// Instanciation of inherited class
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+for($i=1;$i<=40;$i++)
+    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+$pdf->Output();
 	
 }
 
