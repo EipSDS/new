6eea705331a0c25f426f1edbc9cbe77affd4a584
simar0707 <simar0707@outlook.com>
2019-10-02 05:17:12 +0530

    add new files

--- non_fleet_form/test1.php
+++ non_fleet_form/test1.php
@@ -1,11 +1,12 @@
 <?php
 
-echo __DIR__ . '/vendor/autoload.php';
-exit();
-$mpdf = new \Mpdf\Mpdf();
+// Using default PHP curl library
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=7ZFW82HETUNMVPKI4SA27OG9JY30L36B056QR49CD11X85&content=http://givesurance.herokuapp.com/non_fleet_form/non_fleet_form.php&margin=10&html_width=1024&title=My PDF Title');
 
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 
 
 ?>
