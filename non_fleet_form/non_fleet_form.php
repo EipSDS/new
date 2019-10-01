a2f2f40b1e1ac1677bd4ba8ae0b6a1410f99250d
simar0707 <simar0707@outlook.com>
2019-10-02 04:50:24 +0530

    add new files

--- non_fleet_form/non_fleet_form.php
+++ non_fleet_form/non_fleet_form.php
@@ -664,7 +664,7 @@ if($row88>=1){
 
 if(isset($_POST['submit'])){
 	
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 ///include ('.//vendor/autoload.php');
 $mpdf = new \Mpdf\Mpdf();
 
