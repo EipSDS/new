75a6a32102a69dcedd1e1b2e808955e9215d2904
simar0707 <simar0707@outlook.com>
2019-10-02 04:27:09 +0530

    add new files

--- non_fleet_form/test1.php
+++ non_fleet_form/test1.php
@@ -2,6 +2,8 @@
 echo "hello world";
 require_once __DIR__ . '/vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
-mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
 $mpdf->Output();
+
+
 ?>
