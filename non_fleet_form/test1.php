2ef2fc2113dc78547a9772c7df392c4cbc1f8a7f
simar0707 <simar0707@outlook.com>
2019-10-02 05:50:06 +0530

    add new files

--- non_fleet_form/test1.php
+++ non_fleet_form/test1.php
@@ -1,7 +1,7 @@
 <?php
 
 // Using default PHP curl library
-$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=7ZFW82HETUNMVPKI4SA27OG9JY30L36B056QR49CD11X85&content=http://givesurance.herokuapp.com/non_fleet_form/non_fleet_form.php&margin=10&html_width=1024&title=My PDF Title');
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=7ZFW82HETUNMVPKI4SA27OG9JY30L36B056QR49CD11X85&content=http://givesurance.herokuapp.com/non_fleet_form/non_fleet_form.php&html_width=2522&margin=10&title=My+PDF+Title');
 
 $response = curl_exec($ch);
 curl_close($ch);
