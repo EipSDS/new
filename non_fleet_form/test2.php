4ff7054136f32227b111397013243535b19bff56
simar0707 <simar0707@outlook.com>
2019-10-02 23:03:34 +0530

    add new file

--- non_fleet_form/test2.php
+++ non_fleet_form/test2.php
@@ -736,41 +736,22 @@ $pdf->AddPage();
 // writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true)
 
 // create some HTML content
-$html = '<h1>HTML Example</h1>
-Some special characters: &lt; € &euro; &#8364; &amp; è &egrave; &copy; &gt; \\slash \\\\double-slash \\\\\\triple-slash
-<h2>List</h2>
-List example:
-<ol>
-	<li><img src="images/logo_example.png" alt="test alt attribute" width="30" height="30" border="0" /> test image</li>
-	<li><b>bold text</b></li>
-	<li><i>italic text</i></li>
-	<li><u>underlined text</u></li>
-	<li><b>b<i>bi<u>biu</u>bi</i>b</b></li>
-	<li><a href="http://www.tecnick.com" dir="ltr">link to http://www.tecnick.com</a></li>
-	<li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.<br />Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</li>
-	<li>SUBLIST
-		<ol>
-			<li>row one
-				<ul>
-					<li>sublist</li>
-				</ul>
-			</li>
-			<li>row two</li>
-		</ol>
-	</li>
-	<li><b>T</b>E<i>S</i><u>T</u> <del>line through</del></li>
-	<li><font size="+3">font + 3</font></li>
-	<li><small>small text</small> normal <small>small text</small> normal <sub>subscript</sub> normal <sup>superscript</sup> normal</li>
-</ol>
-<dl>
-	<dt>Coffee</dt>
-	<dd>Black hot drink</dd>
-	<dt>Milk</dt>
-	<dd>White cold drink</dd>
-</dl>
-<div style="text-align:center">IMAGES<br />
-<img src="images/logo_example.png" alt="test alt attribute" width="100" height="100" border="0" /><img src="images/tcpdf_box.svg" alt="test alt attribute" width="100" height="100" border="0" /><img src="images/logo_example.jpg" alt="test alt attribute" width="100" height="100" border="0" />
-</div>';
+$html = '	<table width="100%" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #000000;">
+	<tr>
+	<td>
+	<table width="100%" align="center" cellpadding="0" cellspacing="0" bgcolor="#c6d9f1">
+	<tr>
+	<td>
+	<table width="100%" align="center" cellpadding="3" cellspacing="3" style="border-bottom: 1px solid #000000;">
+	<tr>
+	<td align="left" style="font-size: 12px;">				
+	<b>Agency Information</b>
+	</td>
+	</tr>
+	</table>
+	</td>
+	</tr>
+	</table>';
 
 // output the HTML content
 $pdf->writeHTML($html, true, false, true, false, '');
