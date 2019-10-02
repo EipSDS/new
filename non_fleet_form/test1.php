58ac2b7aa967b8ae5d217f7f4f122def1180bc52
simar0707 <simar0707@outlook.com>
2019-10-02 06:19:19 +0530

    add new files

--- non_fleet_form/test1.php
+++ non_fleet_form/test1.php
@@ -3,7 +3,7 @@
 // Using default PHP curl library
 $ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=7ZFW82HETUNMVPKI4SA27OG9JY30L36B056QR49CD11X85&content=http://www.google.com/&margin=10&html_width=1024&title=My PDF Title');
 
-$response = curl_exec($ch);
+echo $response = curl_exec($ch);
 curl_close($ch);
 
 echo $response;
